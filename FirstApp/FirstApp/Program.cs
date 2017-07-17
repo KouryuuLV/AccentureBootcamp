bdafb98aa6f4c6b32e58b7919bcc11c6078eccfd
Juris Lukstins <juris.lukstins@gmail.com>
2017-07-17 16:07:49 +0300

    1st commit
    1st commit

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer between 1 and 3: ");
+
+            string data = Console.ReadLine();
+
+            int myInt;
+
+            if (!int.TryParse(data, out myInt))
+            {
+                data = "you did not enter an integer!";
+            }
+            else
+            {
+                switch (myInt)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                        data = string.Format("Your number is {0}.", myInt);
+                        break;
+                    default:
+                        data = string.Format("Your number is {0} is not between 1 and 3!", myInt);
+                        break;
+                }
+            }
+            System.Console.WriteLine("{0}\nPress Enter to exit...", data);
+            //System.Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
