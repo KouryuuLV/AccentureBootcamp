f4f1f69e283d49691e19dde6373ece09214c4374
Juris Lukstins <juris.lukstins@gmail.com>
2017-07-31 17:30:49 +0300

    Hermes chat
    Hermes Chat

---
+++ ChatHermes/HermesORM/Mapping/UserMap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using HermesORM.DbEntities;
+
+namespace HermesORM
+{
+    public class UserMap : System.Data.Entity.ModelConfiguration.EntityTypeConfiguration<User>
+    {
+        public UserMap()
+        {
+            ToTable("User");
+            HasKey(x => x.UserId);
+
+            Property(x => x.UserId).HasColumnName("UserId").IsRequired().HasColumnType("int").HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+            Property(x => x.UserName).HasColumnName("UserName").IsRequired().HasColumnType("nvarchar").HasMaxLength(50);
+            Property(x => x.FirstName).HasColumnName("FirstName").IsRequired().HasColumnType("nvarchar").HasMaxLength(50);
+            Property(x => x.LastName).HasColumnName("LastName").IsRequired().HasColumnType("nvarchar").HasMaxLength(50);
+            Property(x => x.Email).HasColumnName("Email").IsRequired().HasColumnType("nvarchar").HasMaxLength(50);
+            Property(x => x.Image).HasColumnName("Image").IsRequired().HasColumnType("nvarchar").HasMaxLength(50);
+            Property(x => x.Password).HasColumnName("Password").IsRequired().HasColumnType("nvarchar").HasMaxLength(50);
+            Property(x => x.DisplayName).HasColumnName("DisplayName").IsOptional().HasColumnType("nvarchar"); //.HasMaxLength(50);
+
+            // Foreign keys
+            //HasRequired(a => a.Author).WithMany(b => b.Articles).HasForeignKey(c => c.AuthorId); // FK_Article_ToAuthor
+            //HasMany(t => t.Tags).WithMany(t => t.User).Map(m =>
+            {
+                //    m.ToTable("blgArticleTags");
+                //    m.MapLeftKey("ArticleId");
+                //    m.MapRightKey("TagId");
+                //});
+            }
+        }
+    }
+}
+
---
+++ ChatHermes/HermesWCF/HermesService.cs
@@ -0,0 +1,51 @@
+﻿using ChatHermes.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HermesORM;
+using HermesORM.DbEntities;
+
+namespace HermesWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class HermesService : IHermesService
+    {
+        public List<UserContract> GetAllUsers()
+        {
+            using (var context = new HermesORM.DbHermesContext())
+            {
+                return context.User.Select(a => new UserContract
+                {
+                    UserId = a.UserId,
+                    UserName = a.UserName,
+                    Password = a.Password,
+                    FirstName = a.FirstName,
+                    LastName = a.LastName,
+                    Email = a.Email,
+                    Image = a.Image
+
+                }).ToList();
+
+            }
+        }
+        public void AddUser(UserContract user)
+        {
+            using (var context = new DbHermesContext())
+            {
+                context.User.Add(new HermesORM.DbEntities.User
+                {
+                    UserId = user.UserId,
+                    UserName = user.UserName,
+                    Password = user.Password,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Image = user.Image
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
